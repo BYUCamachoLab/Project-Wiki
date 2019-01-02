79818b388d2dd4a9c7c4cbdf01214de9dd8c0794
Sequoia Ploeg <sequoiap4@gmail.com>
2019-01-02 11:59:33 -0700

    removed local server

--- macosx/start.sh
+++ macosx/start.sh
@@ -17,24 +17,24 @@ else
     echo "MongoDB fail to start"
 fi
 
-# start web app
-nohup python PW_run.py &>/dev/null &
+# # start web app
+# nohup python PW_run.py &>/dev/null &
 
-# start caddy
-nohup caddy -conf PW_Caddyfile &>/dev/null &
-
-
-if pgrep -x "caddy" > /dev/null
-then
-    echo "Caddy started"
-else
-    echo "Caddy fail to start"
-fi
+# # start caddy
+# nohup caddy -conf PW_Caddyfile &>/dev/null &
 
 
-if pgrep -x "mongod" > /dev/null && pgrep -x "caddy" > /dev/null
-then
-    echo "Project Wiki started"
-else
-    echo "Project Wiki fail to start"
-fi
+# if pgrep -x "caddy" > /dev/null
+# then
+#     echo "Caddy started"
+# else
+#     echo "Caddy fail to start"
+# fi
+
+
+# if pgrep -x "mongod" > /dev/null && pgrep -x "caddy" > /dev/null
+# then
+#     echo "Project Wiki started"
+# else
+#     echo "Project Wiki fail to start"
+# fi
