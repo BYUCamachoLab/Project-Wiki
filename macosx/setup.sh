34ee7e1ac84f4ffcb4101d03ccce8c81d46db983
Sequoia Ploeg <sequoiap4@gmail.com>
2019-01-02 10:11:12 -0700

    updated ports

--- macosx/setup.sh
+++ macosx/setup.sh
@@ -45,7 +45,7 @@ from manage import app
 from waitress import serve
 
 
-serve(app, listen='127.0.0.1:31415', threads=4)
+serve(app, listen='127.0.0.1:8080', threads=4)
 EOF
 
 # kill mongod process
